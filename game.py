dd14bba3e041aaff0fd9fed36d80094505732985
Lyan <lyanlyandev@gmail.com>
2022-12-30 21:35:01 +0630

    King Done

--- game.py
+++ game.py
@@ -2,11 +2,11 @@ from abc import ABC, abstractclassmethod
 from string import ascii_lowercase
 
 FILE = ascii_lowercase[:8]
-RANK = list(range(1, 9))
+RANK = "".join(str(x) for x in range(1, 9))
 
 class Piece(ABC):
-    def __init__(self) -> None:
-        self.position = str()
+    def __init__(self, position) -> None:
+        self.position = position
 
     @abstractclassmethod
     def valid_move(self) -> list:
@@ -16,10 +16,29 @@ class Piece(ABC):
     def move(self, new_pos: str):
         if new_pos in self.valid_move:
             self.position = new_pos
-    
+
+    @staticmethod
+    def cell_validaty(pos: str):
+        if pos[0] in FILE and pos[1] in RANK:
+            return True
+        return False    
 
 class King(Piece):
-    pass
+    def valid_move(self) -> list:
+        output = []
+        file_index = FILE.find(self.position[0])
+
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                valid_pos = ascii_lowercase[file_index + i] + str(int(self.position[1]) + j)
+
+                if self.cell_validaty(valid_pos) and valid_pos != self.position:
+                    output.append(valid_pos)
+        
+        return output
+
+    def move(self, new_pos: str):
+        super().move(new_pos)
 
 class Queen(Piece):
     pass 
@@ -36,12 +55,10 @@ class Rooke(Piece):
 class Pawn(Piece):
     pass
 
-white = [
-    *[Pawn() for _ in range(8)],
-    Rooke(), Knight(), Bishop(), Queen(), King(), Bishop(), Knight(), Rooke()
-]
-
-black = white.copy()
+# white = [
+#     *[Pawn("b" + str(x)) for x in range(1, 9)],
+#     Rooke(), Knight(), Bishop(), Queen(), King(), Bishop(), Knight(), Rooke()
+# ]
 
 if "__main__" == __name__:
-    pass
+    print(King("a1").valid_move())
