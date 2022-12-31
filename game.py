9d32d37e259434f179898650b2006e400cb743a5
Lyan <lyanlyandev@gmail.com>
2022-12-31 07:40:23 +0630

    Bishop Done

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractclassmethod
 from string import ascii_lowercase
+import math
 
 FILE = ascii_lowercase[:8]
 RANK = "".join(str(x) for x in range(1, 9))
@@ -8,11 +9,13 @@ class Piece(ABC):
     def __init__(self, position) -> None:
         self.position = position
 
+    def __str__(self):
+        return self.__class__.__name__[0] + self.position
+    
     @abstractclassmethod
     def valid_move(self) -> list:
         pass
 
-    @abstractclassmethod
     def move(self, new_pos: str):
         if new_pos in self.valid_move:
             self.position = new_pos
@@ -37,15 +40,31 @@ class King(Piece):
         
         return output
 
-    def move(self, new_pos: str):
-        super().move(new_pos)
 
 class Queen(Piece):
     pass 
 
 class Bishop(Piece):
-    pass
+    def valid_move(self):
+        output = []
 
+        current_file_index = FILE.find(self.position[0])
+        
+        for file_index in range(8):
+            if current_file_index == file_index:
+                continue
+
+            valid_pos_2 = FILE[file_index] + str(int(self.position[1]) - abs(current_file_index - file_index))
+            valid_pos_1 = FILE[file_index] + str(int(self.position[1]) + abs(current_file_index - file_index))
+
+            if self.cell_validaty(valid_pos_1):
+                output.append(valid_pos_1)
+
+            if self.cell_validaty(valid_pos_2):
+                output.append(valid_pos_2)
+        
+        return output
+        
 class Knight(Piece):
     pass
 
@@ -61,4 +80,6 @@ class Pawn(Piece):
 # ]
 
 if "__main__" == __name__:
-    print(King("a1").valid_move())
+    b = Bishop("f6")
+
+    
